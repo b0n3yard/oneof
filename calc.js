67e9e3ada64178d7c55737e640ee611f0cbb71cf
Michael <namesake39@gmail.com>
2024-12-16 17:36:23 -0500

    added comlex equasion support

--- calc.js
+++ calc.js
@@ -2,26 +2,28 @@ const inquirer = require('inquirer')
 let total = 0
 let fulltotal = 0
 
-const sum = async (num1, num2, sym) =>{
-    console.log("hi")
-    switch(sym){
-        case '+':
-            total = Number(num1) + Number(num2)
-            break;
-        case '-':
-            total = Number(num1) - Number(num2)
-            break;
-        case '*':
-            total = Number(num1) * Number(num2)
-            break;
-        case '/':
-            total = Number(num1) / Number(num2)
-            break;
-        default:
-            console.log('default')
+const sum = async (testing) =>{
+    const symbol =/[-+*/]/g
+    const symbol2 =/[+\-*/]?\d+|\*?\([^)]+\)/g
+    const numbers = /([0-9]*\.?[0-9]+)/g
+    const splits = testing.match(symbol2)
+    console.log(splits)
+    const full = testing.match(symbol)
+    const nums = testing.match(numbers)
+    let lit = `total = ${total}`
+    console.log(nums.length)
+    if(fulltotal == 0){
+         lit = `total = ${splits[0]}${splits[1]}`
+    }else{
+        for(let x in nums){
+            lit= lit +`${splits[x]}` 
+        }
     }
+    console.log(lit)
+    eval(lit)
     
-    fulltotal += total
+    fulltotal = total
+    lit = 0
     console.log(fulltotal)
     show()
 }
@@ -31,39 +33,16 @@ const another = async () =>{
         name: 'nwnum',
         message: 'new equasion'
     })
-    const symbol =/[-+*/]/g
-    const numbers = /([0-9]*\.?[0-9]+)/g
-    const full = nwnum.match(symbol)
-    const nums = nwnum.match(numbers)
-    const symblmtch =  full ? full[0] : null
-    console.log(symblmtch)
-    console.log(typeof symblmtch)
-    console.log(Symbol(symblmtch))
-    total = fulltotal
-    console.log(full)
-    console.log("nums", nums[0])
-           const operation = `total ${symblmtch}= ${Number(nums[0])}`
-           console.log(operation)
-           eval(operation)
-   
-    fulltotal = total
-    console.log(fulltotal)
-    return( fulltotal, show())
+    sum(nwnum)
+    
 }
-const test = async () =>{
+const welcome = async () =>{
         const {testing} = await inquirer.prompt({
             name: 'testing',
             message: 'enter equasion'
         })
-        const symbol =/[-+*/]/g
-        const numbers = /([-+]?[0-9]*\.?[0-9]+)/g
-        const full = testing.match(symbol)
-        const nums = testing.match(numbers)
+        sum(testing)
         
-        console.log(full)
-        console.log(nums)
-        const choice = full[0]
-        sum(nums[0], nums[1], full[0])
         
 }
 
@@ -72,8 +51,9 @@ const show = async () =>{
         name:"intro",
         message: " what would you like to do?",
         type: 'list',
-        choices: ['add', 'clear','test','add another','display','leave']
+        choices: ['add another', 'clear','display','leave']
     })
+    // console.log(choice)
     switch(choice.intro){
         case 'add':
             sum()
@@ -84,16 +64,18 @@ const show = async () =>{
         case 'clear':
             fulltotal = 0
             show()
+            break;
         case 'test':
             test()
             break;
         case 'display':
             console.log(fulltotal)
             show()
+            break;
         default:
             break;
     }
 
 
 }
-show()
+welcome()
--- testbench.js
+++ testbench.js
@@ -1,12 +1,17 @@
 let total = 0
 let paretotal = 0
 const parenthesesRegex =  /\(([^)]+)\)/g
-
+const symbol =/[-+*/]/g
+// /[-+*/]/g
+const symbol2 =/[+\-*/]?\d+|\*?\([^)]+\)/g
 const operation2 = `total = 4+4-2*(5 + 5)+(3+3)`
+const slen = operation2.replace(`total =`,``).trim()
+const splits = slen.match(symbol2)
+
+console.log("splits", splits)
 const match = [...operation2.matchAll(parenthesesRegex)]
 const matches = match.map(match => match[1].match(/\d+(\.\d+)?/g)).flat()
 console.log("match",matches)
-const symbol =/[-+*/]/g
 const sym = match[0][1].match(symbol)
 const pare = `paretotal = ${matches[0]}${sym[0]}${matches[1]}`
 eval(pare)
